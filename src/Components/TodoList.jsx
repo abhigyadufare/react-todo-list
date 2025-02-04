41baf0f6ac961d2cc2f7ca83adcbc0268b79524e
abhigya <abhigyadufare@gmail.com>
2025-02-04 17:16:12 -0500

    added functionality to create lists for a particular todo list heading

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -11,6 +11,19 @@ const TodoList = () => {
         setTodos([...todos, {heading: headingInput, lists: []}]);
         setHeadingInput("");
     }
+  };
+
+  const handleAddList = (index) => {
+    if (listInputs[index] && listInputs[index].trim !== "") {
+        const newTodos = [...todos];
+        newTodos[index].lists.push(listInputs[index]);
+        setTodos(newTodos);
+        setListInputs({...listInputs, [index]: ""});
+    }
+  };
+
+  const handleListInputChange = (index, value) => {
+    setListInputs({...listInputs, [index]: value});
   }
 
   return (
@@ -34,6 +47,23 @@ const TodoList = () => {
                 <div className="heading_todo">
                     <h3>{todo.heading}</h3> {/* Display the heading here */}
                     <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading</button>
+                    <ul>
+                        {todo.lists.map((list, listIndex) => (
+                            <li key={listIndex} className="todo_inside_list">
+                                <p>{list}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="add-list">
+                        <input 
+                            type="text"
+                            className="list-input"
+                            placeholder="Add List"
+                            value={listInputs[index] || ""}
+                            onChange={(e) => handleListInputChange(index, e.target.value)}
+                        />
+                        <button className="add-list-button" onClick={() => handleAddList(index)}>Add List</button>
+                    </div>
                 </div>
             </div>
         ))}
